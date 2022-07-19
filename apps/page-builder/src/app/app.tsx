0abe176c23b61b4369991440715b1470a6090dc0
fancn21th <fancn21th@gmail.com>
2022-07-19 18:26:07 +0800

    scss style update

--- apps/page-builder/src/app/app.tsx
+++ apps/page-builder/src/app/app.tsx
@@ -1,6 +1,3 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from './app.module.scss';
-
 import { Route, Routes, Link } from 'react-router-dom';
 import Button from '@mui/material/Button';
 
