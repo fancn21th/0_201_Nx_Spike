bbe13ea22305d7e16cd682e37a501021665e2fa0
fancn21th <fancn21th@gmail.com>
2022-07-21 12:30:48 +0800

    first cypress test

--- apps/page-builder-ui-shared-e2e/src/integration/header/header.spec.ts
+++ apps/page-builder-ui-shared-e2e/src/integration/header/header.spec.ts
@@ -1,7 +1,7 @@
 describe('page-builder-ui-shared: Header component', () => {
   beforeEach(() => cy.visit('/iframe.html?id=header--primary'));
-    
-    it('should render the component', () => {
-      cy.get('h1').should('contain', 'Welcome to Header!');
-    });
+
+  it('should render the component', () => {
+    cy.get('.css-1t6c9ts > :nth-child(1)').should('contain', 'List Page');
+  });
 });
